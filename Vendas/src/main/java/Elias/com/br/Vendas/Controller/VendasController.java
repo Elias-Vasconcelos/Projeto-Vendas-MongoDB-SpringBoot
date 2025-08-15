b16b7c962b88ae8dfa653e3a6f3c1f08a91a4164
Elias Ferreira <146135801+Elias-Vasconcelos@users.noreply.github.com>
2025-08-15 14:28:07 -0300

    Inicio controller microsserviço de Vendas

--- Vendas/src/main/java/Elias/com/br/Vendas/Controller/VendasController.java
+++ Vendas/src/main/java/Elias/com/br/Vendas/Controller/VendasController.java
@@ -1,4 +1,60 @@
-package Elias.com.br.Vendas.Controller;
+    package Elias.com.br.Vendas.Controller;
 
-public class VendasController {
-}
+    import Elias.com.br.Vendas.Domain.Vendas;
+    import Elias.com.br.Vendas.UseCase.*;
+    import io.swagger.v3.oas.annotations.Operation;
+    import jakarta.validation.Valid;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.web.bind.annotation.*;
+
+    import java.util.Optional;
+
+    @RestController
+    @RequestMapping("/Vendas")
+    public class VendasController {
+
+        private AdicionarProdutoCase adicionarProduto;
+        private BuscaVendaCase buscar;
+        private CancelarVenda cancelarVenda;
+        private ConcluirVendaCase concluirVenda;
+        private CriarVendaCase criarVenda;
+        private RemoverProdutoCase removerProduto;
+
+
+        @Autowired
+        public VendasController(AdicionarProdutoCase adicionarProduto, BuscaVendaCase buscar, CancelarVenda cancelarVenda, ConcluirVendaCase concluirVenda, CriarVendaCase criarVenda, RemoverProdutoCase removerProduto) {
+            this.adicionarProduto = adicionarProduto;
+            this.buscar = buscar;
+            this.cancelarVenda = cancelarVenda;
+            this.concluirVenda = concluirVenda;
+            this.criarVenda = criarVenda;
+            this.removerProduto = removerProduto;
+        }
+
+
+        @PostMapping
+        @Operation(summary = "cadastrar uma Venda")
+        public void cadastrarVenda(@RequestBody @Valid String clientId){
+            criarVenda.CriarVenda(clientId);
+        }
+
+        @GetMapping(value = "/ID/{id}")
+        @Operation(summary = "Busca uma Venda pelo ID")
+        public void buscarVenda( @PathVariable(value = "ID", required = true) String vendaId){
+            buscar.buscarPorId(vendaId);
+        }
+
+        @PutMapping
+        @Operation(summary = "Adiciona um produto a uma venda")
+        public void  adicionarProdotoVenda(@RequestBody @Valid String vendaId, String produtoId){
+            adicionarProduto.adicionarProduto(vendaId, produtoId);
+        }
+
+        @PutMapping
+        @Operation(summary = "Remove um produto a uma venda")
+        public  void removerProdutoVenda(@RequestBody @Valid String vendaId, String produtoId){
+            removerProduto.RemoveProdutoVenda(vendaId, produtoId);
+        }
+
+
+    }
