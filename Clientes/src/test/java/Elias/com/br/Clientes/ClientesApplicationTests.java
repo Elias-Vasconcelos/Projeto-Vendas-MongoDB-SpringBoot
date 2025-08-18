393674b498d40e930ebd77d92d84ec0f66590866
Elias Ferreira <146135801+Elias-Vasconcelos@users.noreply.github.com>
2025-08-18 12:21:16 -0300

    Inicio dos testes microsserviço de Clientes

--- Clientes/src/test/java/Elias/com/br/Clientes/ClientesApplicationTests.java
+++ Clientes/src/test/java/Elias/com/br/Clientes/ClientesApplicationTests.java
@@ -3,7 +3,7 @@ package Elias.com.br.Clientes;
 import Elias.com.br.Clientes.Domain.Clientes;
 import Elias.com.br.Clientes.controller.ClientesController;
 import Elias.com.br.Clientes.repository.IClientesRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,29 +12,66 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ClientesApplicationTests {
 
 	@Autowired
-	private IClientesRepository clientesRepository;
-
-	@Autowired
-	private ClientesController controller;
+	static ClientesController controller;
 
 	@Test
 	void contextLoads() {
 		assertThat(controller).isNotNull();
-		assertThat(clientesRepository).isNotNull();
 	}
 
-//	@Test
-//	public void cadastroCliente() {
-//		Clientes clienteTest = new Clientes(
-//				"1223", "Elias", 155523456,
-//				"Eliaaas@gmail.com", 9123456,
-//				"Teste", 10, "TestCit", "Sp"
-//		);
-//
- //		Clientes salvo = controller.cadastrar(clienteTest);
-//
-//
-//	}
+	static Clientes clienteTest = new Clientes();
+
+
+	@BeforeEach
+	public void cadastroCliente() {
+
+		clienteTest.setNome("Elias");
+		clienteTest.setCpf(123456789);
+		clienteTest.setEmail("Elias@gmail.com");
+		clienteTest.setEnd("Rua Test");
+		clienteTest.setNumero(999);
+		clienteTest.setEstado("São paulo");
+		clienteTest.setCidade("São paulo");
+		clienteTest.setTel(1199999999);
+		controller.cadastrarCliente(clienteTest);
+
+	}
+
+
+	@AfterEach
+	public void removerCliente() {
+		controller.deletarCliente(clienteTest);
+	}
+
+
+
+	@Test
+	public void testBuscarCliente(){
+		Clientes cliente = controller.buscarClientePorCpf(123456789).orElseThrow(() ->
+				new RuntimeException("Cliente não encontrada com o Cpf: " + clienteTest.getCpf()));
+
+
+		assertThat(cliente).isNotNull();
+
+	}
+
+
+	@Test
+	public  void testAlterarCliente(){
+
+        controller.alterarClienteEmail("011",  "Email.atualizado");
+		controller.alterarClienteNome("011", "NomeAtualizado" );
+		controller.alterarClienteTelefone("011", 119111111);
+		controller.alterarClienteEnd("011", "RuaAtualizada");
+
+		Clientes cliente = controller.buscarClientePorId("011").orElseThrow(() ->
+					new RuntimeException("Cliente não encontrada com o id fornecido"));
+
+		assertThat(cliente.getEmail()).isEqualTo("Email.atualizado");
+		assertThat(cliente.getNome()).isEqualTo("NomeAtualizado");
+		assertThat(cliente.getTel()).isEqualTo(119111111);
+		assertThat(cliente.getEnd()).isEqualTo("RuaAtualizada");
+	}
 
 
 //	@Test
